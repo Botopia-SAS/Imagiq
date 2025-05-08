692dbea0e2a18f99ba4d291e27be262dd13dbf74
Botopia Technology SAS <botopia.sas@gmail.com>
2025-05-07 23:52:19 -0500

    Update page.tsx

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -32,7 +32,7 @@ export default function ARScene() {
 
         const model = gltf.scene;
         model.position.set(0, 0, -0.5); // Frente a la cámara
-        model.scale.set(0.01, 0.01, 0.01); // Escala ajustable
+        model.scale.set(0.001, 0.001, 0.001); // Escala ajustable
         scene.add(model);
       });
 
